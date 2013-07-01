76d530c3e0e21a143ef8cc4c9814a647595f7ddf
Ramy Kfoury <ramy.kfoury@foo-solutions.com>
2013-07-01 11:38:46 +0300

    Default Project Structure
    Structure of Project: Models, Views, Controllers, in addition to Cells,
    Slices and Helpers

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Ramy Kfoury on 7/1/13.
+//  Copyright (c) 2013 Ramy Kfoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ demo/demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo
+//
+//  Created by Ramy Kfoury on 7/1/13.
+//  Copyright (c) 2013 Ramy Kfoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
